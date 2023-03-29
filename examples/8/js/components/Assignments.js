7ae4592086ac2c0d24ef6a755cc8d8c4a9436db7
pkarnachoritis <63738599+panoskarnachoritis@users.noreply.github.com>
2023-03-29 20:05:47 +0300

    lesson 8

---
+++ examples/8/js/components/Assignments.js
@@ -0,0 +1,51 @@
+import  AssignmentList from "./AssignmentList.js";
+
+export default  {
+    components: { AssignmentList },
+
+    template: `
+        <section class="space-y-6">
+          <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
+          <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+
+          <form @submit.prevent="add">
+            <div class="border border-gray-600 text-black">
+                <input v-model="newAssignment" placeholder="New assignment..." class="p-2">
+                <button type="submit" class="bg-white p-2 border-l">Add</button>  
+            </div>
+          </form>
+        </section>
+    `,
+
+    data() {
+        return {
+            assignments: [
+                { name: 'Finish Project', complete: false, id: 1 },
+                { name: 'Read Chapter', complete: false, id: 2 },
+                { name: 'Repeat', complete: false, id: 3 },
+            ],
+
+            newAssignment: ''
+        }
+    },
+
+    computed: {
+        filters() {
+            return {
+                inProgress: this.assignments.filter(assignment => ! assignment.complete),
+                completed: this.assignments.filter(assignment => assignment.complete)
+            }
+        }
+    },
+
+    methods: {
+        add() {
+            this.assignments.push({
+                name: this.newAssignment,
+                complete: false,
+                id: this.assignments.length + 1
+            });
+            this.newAssignment = '';
+        }
+    }
+}
