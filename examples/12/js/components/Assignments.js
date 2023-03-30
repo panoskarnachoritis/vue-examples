0fb37c9f884705e42d76d99b068feb89b461f348
pkarnachoritis <63738599+panoskarnachoritis@users.noreply.github.com>
2023-03-30 04:27:14 +0300

    lesson 12

---
+++ examples/12/js/components/Assignments.js
@@ -0,0 +1,44 @@
+import  AssignmentList from "./AssignmentList.js";
+import AssignmentCreate from "./AssignmentCreate.js";
+
+export default  {
+    components: { AssignmentList, AssignmentCreate },
+
+    template: `
+        <section class="space-y-6">
+          <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
+          <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+
+          <assignment-create @add="add"></assignment-create>
+        </section>
+    `,
+
+    data() {
+        return {
+            assignments: [
+                { name: 'Finish Project', complete: false, id: 1, tag: 'math' },
+                { name: 'Read Chapter', complete: false, id: 2, tag: 'science' },
+                { name: 'Repeat', complete: false, id: 3, tag: 'math' },
+            ],
+        }
+    },
+
+    computed: {
+        filters() {
+            return {
+                inProgress: this.assignments.filter(assignment => ! assignment.complete),
+                completed: this.assignments.filter(assignment => assignment.complete)
+            }
+        }
+    },
+
+    methods: {
+        add(name) {
+            this.assignments.push({
+                name: name,
+                complete: false,
+                id: this.assignments.length + 1
+            });
+        }
+    }
+}
