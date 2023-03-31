7a34defe8477e866ebca902b47e8abc2a647f5b6
pkarnachoritis <63738599+panoskarnachoritis@users.noreply.github.com>
2023-03-31 15:39:13 +0300

    lesson 18

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,30 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import {ref} from 'vue'
+
+let message = ref("Hello world");
+
+let doSomething = () => {
+  alert("doing it now");
+}
+
+setTimeout(() => {
+  message.value = "Changed";
+}, 5000);
 </script>
 
 <template>
   <main>
     <TheWelcome />
+
+    <p>{{ message }}</p>
+    <p>
+      <input type="text" v-model="message">
+    </p>
+
+    <p>
+      <button @click="doSomething">Click Me</button>
+    </p>
+
   </main>
 </template>
